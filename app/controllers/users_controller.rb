1c13e0c79dd61a188b4c4a037e0da799d711d4d6
Aleksandra <aleksandra@wroblewscy.eu>
2023-07-27 12:04:09 +0200

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])    
+  end
+
+  private
+  def user_params       
+    params.require(:user).permit(:email, :password)
+  end
+end
