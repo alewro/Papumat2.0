8c4502acbceb4f2afd70c15e51027cf6e390c5dc
Aleksandra <aleksandra@wroblewscy.eu>
2023-07-27 13:10:22 +0200

    Add recipe scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_090553) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_101340) do
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
