a52b4cd823c20e621c1804c7d61677ffabe46c1c
Aleksandra <aleksandra@wroblewscy.eu>
2023-08-05 15:24:12 +0200

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#User create
+User.create(email: "test@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+#Recipe Categories create
+RecipeCategory.create(name:"SaltyBreakfast")
+RecipeCategory.create(name:"SweetBreakfast")
+RecipeCategory.create(name:"Lunch")
+RecipeCategory.create(name:"OneDick")
+
+#Product Categories create
+ProductCategory.create(name:"diary")
+ProductCategory.create(name:"vegetables")
+ProductCategory.create(name:"fruit")
+ProductCategory.create(name:"bread")
+
+#Recipes create
+10.times do |x|
+  Recipe.create(name: "#{x} recipe", description: "Description goes brrr for #{x} time", recipe_category_id: 1)
+  Recipe.create(name: "0#{x} recipe", description: "Description goes brrr for #{x} time", recipe_category_id: 2)
+  Recipe.create(name: "1#{x} recipe", description: "Description goes brrr for #{x} time", recipe_category_id: 3)
+  Recipe.create(name: "2#{x} recipe", description: "Description goes brrr for #{x} time", recipe_category_id: 4)
+end
